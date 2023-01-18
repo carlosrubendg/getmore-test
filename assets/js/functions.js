c69ba413ce736e00ddbaadcc38e79e25efc821a2
Carlos Díaz <dg.carlosr@gmail.com>
2023-01-18 15:12:11 -0600

    First commit

---
+++ assets/js/functions.js
@@ -0,0 +1,5 @@
+function handleToggleBars() {
+  document.querySelector('body').classList.toggle("Overflow");
+  document.querySelector('#HeaderMobile').classList.toggle('Inactive');
+  document.querySelector('.fa-solid').classList.toggle("fa-xmark");
+}
